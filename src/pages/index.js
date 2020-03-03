7128dd5203d833f6a04fe2d643dc5d9a8b934537
Patryk Keska <patrykkeska4@gmail.com>
2020-03-03 20:12:52 +0100

    nip

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,27 @@
 import React from "react"
 import SEO from "../components/seo"
+import styled from 'styled-components'; 
+
+
+const StyledWrapper = styled.div`
+width : 100%; 
+height: 100vh; 
+background-color : rgba(0,0,0,.6); 
+padding : 200px; 
+
+h1{ 
+  margin :0 ;
+}
+`
 
 
 const IndexPage = () => (
-  <>
+  <StyledWrapper>
     <SEO title="Home" />
-    <h1>Hi  people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Strona w przebydowie</h1>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
     </div>
-  </>
+  </StyledWrapper>
 )
 
 export default IndexPage
